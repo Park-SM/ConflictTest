2f97d433650b242332190d70c173287887d600ea
ParkSM <park97.sm@gmail.com>
2021-11-19 11:43:55 +0900

    first commit

---
+++ app/src/main/java/com/smparkworld/conflicttest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smparkworld.conflicttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
