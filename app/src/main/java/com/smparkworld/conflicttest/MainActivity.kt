f5441ca36d2fb6cf071b393904905c0bad75b570
ParkSM <park97.sm@gmail.com>
2021-11-19 11:45:01 +0900

    add toast message

--- app/src/main/java/com/smparkworld/conflicttest/MainActivity.kt
+++ app/src/main/java/com/smparkworld/conflicttest/MainActivity.kt
@@ -2,10 +2,13 @@ package com.smparkworld.conflicttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show()
     }
 }
